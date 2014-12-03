e90dc386f3c0d7878546666839c51756bc107ffb
Jun Woong <woong.jun@gmail.com>
2014-12-04 08:46:21 +0900

    Added test cases for performance check

---
+++ tst/perf/perf-pp-mexp-3.c
@@ -0,0 +1,27 @@
+#define X0 X1 X1
+#define X1 X2 X2
+#define X2 X3 X3
+#define X3 X4 X4
+#define X4 X5 X5
+#define X5 X6 X6
+#define X6 X7 X7
+#define X7 X8 X8
+#define X8 X9 X9
+#define X9 X10 X10
+#define X10 X11 X11
+#define X11 X12 X12
+#define X12 X13 X13
+#define X13 X14 X14
+#define X14 X15 X15
+#define X15 X16 X16
+#define X16 X17 X17
+#define X17 X18 X18
+#define X18 X19 X19
+#define X19 X20 X20
+#define X20 X21 X21
+#define X21 X22 X22
+#define X22 X23 X23
+
+#define x(a)
+
+x(X0)
