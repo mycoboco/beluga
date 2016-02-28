c7314ee8ec472e3028a3b4f95c765b766ef39fa8
Jun Woong <woong.jun@gmail.com>
2016-02-28 14:32:30 +0900

    Added test cases for #42

---
+++ tst/diag/d-42.c
@@ -0,0 +1,32 @@
+/* -v */
+
+f1(void) {}
+f2(void) { return; }
+
+int f3(void) {}
+int f4(void) { return; }
+
+typedef NONE;
+NONE f5(void) {}
+NONE f6(void) { return; }
+
+typedef int INT;
+INT f7(void) {}
+INT f8(void) { return; }
+
+void f9(void) {}
+void f10(void) { return; }
+
+typedef void VOID;
+VOID f11(void) {}
+VOID f12(void) { return; }
+
+const f13(void) {}
+const f14(void) { return; }
+
+typedef const CONST;
+CONST f15(void) {}
+CONST f16(void) { return; }
+
+static f17(void) {}
+static f18(void) { return; }
