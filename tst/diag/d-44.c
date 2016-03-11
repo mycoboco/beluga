825fec53d69ffa8fa43321d6d9c47e7b9cfd4d17
Jun Woong <woong.jun@gmail.com>
2016-03-12 08:32:19 +0900

    Added a test case for #44

---
+++ tst/diag/d-44.c
@@ -0,0 +1,18 @@
+/* --parsable -W */
+# 1 "d-in-internal-12.c"
+# 1 "foo.c" 1
+test
+
+# 5 "foo.c"
+test
+# 2 "d-in-internal-12.c" 2
+
+test
+
+# 7 "d-in-internal-12.c"
+test
+
+# 1 "bar.c" 1
+test
+
+# 10 "d-in-internal-12.c" 2
