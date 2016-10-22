44d3e745eb23a59da7cc82d7e394149fb2f19650
Jun Woong <woong.jun@gmail.com>
2016-10-23 00:12:29 +0900

    feat(new-lexer): add files missing from b90c7373951fb1b3f4df4c0313a7ed540ca77f60 (#78)

---
+++ lib/inc.h
@@ -0,0 +1,37 @@
+/*
+ *
+ */
+
+#ifndef INC_H
+#define INC_H
+
+#include <stdio.h>    /* FILE */
+
+#include "common.h"
+#include "lmap.h"
+
+
+/* #include chain element */
+typedef struct inc_t {
+    FILE *fptr;             /* file pointer */
+    // cond_t *cond;           /* context for conditional inclusion */
+    int mgstate;            /* mg_state */
+    const char *mgname;     /* mg_name */
+} inc_t;
+
+
+extern inc_t **inc_chain;    /* #include chain */
+
+
+void inc_add(const char *, int);
+void inc_init(void);
+void inc_free(void);
+int inc_start(const char *, const lmap_t *);
+void inc_push(FILE *);
+FILE *inc_pop(FILE *, sz_t *);
+int inc_isffile(void);
+
+
+#endif    /* INC_H */
+
+/* end of inc.h */
