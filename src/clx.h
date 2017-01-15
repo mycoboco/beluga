85e0b1c151566d6dcabdb4605e10ddd5f5032cc1
Jun Woong <woong.jun@gmail.com>
2017-01-15 17:01:00 +0900

    fix(clx): fix to correctly distinguish declarations from statements (#82)

--- src/clx.h
+++ src/clx.h
@@ -31,21 +31,23 @@ extern const char clx_kind[];            /* token kinds */
 #define clx_peek() (lst_peekns()->id)    /* looks ahead next token */
 
 /* checks if token denotes type */
-#define clx_istype(s) (clx_kind[clx_tc] == LEX_CHAR ||                                         \
-                       (clx_tc == LEX_ID && ((clx_sym && clx_sym->sclass == LEX_TYPEDEF) ||    \
-                                             (!clx_sym && clx_tyla(s)))))
+#define clx_tykw()    (clx_kind[clx_tc] == LEX_CHAR)
+#define clx_tyid(s)   (clx_tc == LEX_ID &&    \
+                       ((clx_sym && clx_sym->sclass == LEX_TYPEDEF) || (!clx_sym && clx_tyla(s))))
+#define clx_istype(s) (clx_tykw() || clx_tyid(s))
 
 /* macros to check current token kind */
 #define clx_isadcl()   (clx_tc == LEX_ID || clx_tc == '*' || clx_tc == '(' || clx_tc == '[')
 #define clx_isdcl()    (clx_tc == LEX_ID || clx_tc == '*' || clx_tc == '(')
 #define clx_ispdcl()   (clx_tc == '*' || clx_tc == '(' || clx_tc == '[')
 #define clx_issdecl(s) (clx_kind[clx_tc] == LEX_STATIC || clx_istype(s))             /* start */
-#define clx_ispdecl()  (clx_issdecl(CLX_TYLA) && clx_peek() != ':')                  /* pure */
+#define clx_ispdecl()  (clx_kind[clx_tc] == LEX_STATIC || clx_tykw() ||    \
+                        (clx_tyid(CLX_TYLA) && clx_peek() != ':'))                   /* pure */
 #define clx_isparam(a) (clx_issdecl(CLX_TYLAP a))
 #define clx_isexpr()   (clx_kind[clx_tc] == LEX_ID)
 #define clx_issstmt()  (clx_kind[clx_tc] == LEX_ID || clx_kind[clx_tc] == LEX_IF)    /* start */
-#define clx_ispstmt()  (clx_issstmt() &&    \
-                        (!clx_istype(CLX_TYLA) || clx_peek() == ':'))                /* pure */
+#define clx_ispstmt()  (clx_issstmt() && !clx_tykw() &&    \
+                        (!clx_tyid(CLX_TYLA) || clx_peek() == ':'))                  /* pure */
 
 /* type look-ahead token sets */
 #define CLX_TYLA  "\x1E*"     /* default; id, * */
