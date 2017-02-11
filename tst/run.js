0d857de49c77d438014bd025232dc0444a9a695a
Jun Woong <woong.jun@gmail.com>
2017-02-11 17:35:07 +0900

    chore(run): update --won options

--- tst/run.js
+++ tst/run.js
@@ -16,7 +16,7 @@ var excludes = {}
 var fails = []
 var copts = [ '--errstop=0', '--no-warncode', '--unwind-typedef',
               '--won=4',   '--won=5',   '--won=18',  '--won=23',  '--won=47', '--won=71',
-              '--won=129', '--won=159', '--won=198', '--won=211', '--won=236' ]
+              '--won=129', '--won=159', '--won=198', '--won=211', '--won=238' ]
 var run = {
     'beluga\'s diagnostics': {
         proc:  diagout,
