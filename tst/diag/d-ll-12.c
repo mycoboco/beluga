6da41d7fab9b42998e375f63f28be1e495331917
Jun Woong <woong.jun@gmail.com>
2017-07-06 19:15:24 +0900

    test(diag): update test cases for 2ae5d63564258d9291db1b17d6ace0b0278919c6

---
+++ tst/diag/d-ll-12.c
@@ -0,0 +1,17 @@
+long long x1;
+long signed int long x2;
+long long double x3;
+unsigned signed long long x4;
+long long char x5;
+char long long x6;
+
+typedef long long ll1;
+ll1 x7;
+long long ll1 x8;
+
+typedef long long ll2;
+ll2 long long x9;
+
+const ll2 long long x10;
+const volatile ll2 long long x11;
+const ll2 long x12;
