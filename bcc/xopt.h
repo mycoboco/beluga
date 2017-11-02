d8dfbaf5788c86362be4958026b5b1a8bf13546a
Jun Woong <woong.jun@gmail.com>
2017-11-02 18:43:32 +0900

    feat(bcc): add -extension option to control non-conforming extensions

--- bcc/xopt.h
+++ bcc/xopt.h
@@ -28,6 +28,9 @@ xx("isystem ?",   "--include-system $", NULL, NULL, "<dir>",             "add <d
 xx("idirafter ?", "--include-after $",  NULL, NULL, "<dir>",             "add <dir> to the end of the include path")
 xx("nostdinc",    "--nostdinc",         NULL, NULL, NULL,                "don't search system include path")
 
+tt("The following options control compilation:")
+xx("extension", "-X", NULL, NULL, NULL, "enable non-conforming extensions")
+
 tt("The following options control linking:")
 xx("L?", NULL, "-L $", NULL, "<dir>", "add <dir> to the end of the library path")
 
