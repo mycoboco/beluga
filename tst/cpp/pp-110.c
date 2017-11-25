b23a27bc3457917d01913d7db131f52b293473ea
Jun Woong <woong.jun@gmail.com>
2017-11-25 21:22:44 +0900

    test(cpp): add test cases for 1da86156fa6df1817d7a28e22f76ca4677721d2c

---
+++ tst/cpp/pp-110.c
@@ -0,0 +1,16 @@
+/* -Wv3 */
+
+/* missing */ "foo??/"
+              "foo??/""
+/* missing */ "??/"
+              "??/""
+              "??/
+"
+
+/* missing */ 'foo??/'
+              'foo??/''
+/* missing */ '??/'
+              '??/''
+              '??/
+'
+/* missing */ '??/
