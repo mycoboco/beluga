b1fc34f72eb0d74a161d2c520b0d442ea9f987c9
Jun Woong <woong.jun@gmail.com>
2017-12-12 12:59:16 +0900

    chore(build/limits.h): use numerical predefined macros (#115)

--- build/linux-x86-64/limits.h
+++ build/linux-x86-64/limits.h
@@ -5,8 +5,12 @@
 #ifndef _LIMITS
 #define _LIMITS
 
+/* signed char properties */
+#define SCHAR_MAX __SCHAR_MAX__
+#define SCHAR_MIN (-SCHAR_MAX-1)
+
 /* char properties */
-#define CHAR_BIT 8
+#define CHAR_BIT __CHAR_BIT__
 #if __CHAR_UNSIGNED__
     #define CHAR_MAX UCHAR_MAX
     #define CHAR_MIN 0
@@ -15,29 +19,34 @@
     #define CHAR_MIN SCHAR_MIN
 #endif    /* __CHAR_UNSIGNED__ */
 
+/* short properties */
+#define SHRT_MAX __SHRT_MAX__
+#define SHRT_MIN (-SHRT_MAX-1)
+
 /* int properties */
-#define INT_MAX  2147483647
-#define INT_MIN  (-INT_MAX-1)
-#define UINT_MAX 4294967295
+#define INT_MAX __INT_MAX__
+#define INT_MIN (-INT_MAX-1)
 
 /* long properties */
-#define LONG_MAX 2147483647L
+#define LONG_MAX __LONG_MAX__
 #define LONG_MIN (-LONG_MAX-1)
+
+#ifdef __LONG_LONG_MAX__
+/* long long properties */
+#define LLONG_MAX __LONG_LONG_MAX__
+#define LLONG_MIN (-LLONG_MAX-1)
+#endif    /* __LONG_LONG_MAX__ */
+
+/* unsigned properties */
+#define UCHAR_MAX  (SCHAR_MAX*2 + 1)
+#define USHRT_MAX  (SHRT_MAX*2 + 1)
+#define UINT_MAX   (INT_MAX*2U + 1)
+#define ULONG_MAX  (LONG_MAX*2UL + 1)
+#ifdef __LONG_LONG_MAX__
+#define ULLONG_MAX (LLONG_MAX*2ULL + 1)
+#endif    /* __LONG_LONG_MAX__ */
 
 /* multibyte properties */
 #define MB_LEN_MAX 16
-
-/* signed char properties */
-#define SCHAR_MAX 127
-#define SCHAR_MIN (-SCHAR_MAX-1)
-
-/* short properties */
-#define SHRT_MAX 32767
-#define SHRT_MIN (-SHRT_MAX-1)
-
-/* unsigned properties */
-#define UCHAR_MAX 255
-#define ULONG_MAX 4294967295UL
-#define USHRT_MAX 65535
 
 #endif    /* _LIMITS */
